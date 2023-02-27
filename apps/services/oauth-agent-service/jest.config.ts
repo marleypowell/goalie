13a614205ff52c8eb7d1714cabdf3324d62e153c
Marley Powell <marley.powell@exclaimer.com>
2023-02-27 11:36:27 +0000

    feat: :sparkles: created initial `oauth-agent-service`

---
+++ apps/services/oauth-agent-service/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'oauth-agent-service',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/services/oauth-agent-service',
+};
