f192f4b1d1405ed164be122c726d8c37c829c75d
Marley Powell <marley.powell@exclaimer.com>
2023-03-06 16:59:23 +0000

    feat: :sparkles: implemented initial login logic

--- apps/app1/src/app/app.component.spec.ts
+++ apps/app1/src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterTestingModule],
+      imports: [AppComponent, RouterTestingModule, HttpClientTestingModule],
     }).compileComponents();
   });
 
@@ -14,18 +14,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'app1'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('app1');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome app1');
   });
 });
--- apps/app1/src/app/app.component.ts
+++ apps/app1/src/app/app.component.ts
@@ -1,14 +1,74 @@
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
-import { Component } from '@angular/core';
+import { AsyncPipe, JsonPipe, NgIf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { HeaderComponent } from '@goalie/ui';
+import { BehaviorSubject, filter, switchMap } from 'rxjs';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, HeaderComponent, AsyncPipe, JsonPipe, NgIf],
   selector: 'sea-project-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'app1';
+export class AppComponent implements OnInit {
+  public readonly authState$ = new BehaviorSubject<any | null>(null);
+
+  public readonly userInfo$ = new BehaviorSubject<any | null>(null);
+
+  public constructor(private readonly http: HttpClient, private readonly router: Router) {}
+
+  public ngOnInit(): void {
+    this.router.events
+      .pipe(
+        filter((e) => e instanceof NavigationEnd),
+        switchMap(() =>
+          this.http.post<{
+            handled: boolean;
+            isLoggedIn: boolean;
+          }>(
+            'http://localhost:3334/api/login/end',
+            { pageUrl: location.href },
+            {
+              withCredentials: true,
+            }
+          )
+        )
+      )
+      .subscribe((res) => {
+        console.log(res);
+        this.authState$.next(res);
+
+        if (res.handled) {
+          history.replaceState({}, document.title, '/');
+        }
+
+        this.updateUserInfo();
+      });
+  }
+
+  public login(): void {
+    this.http
+      .post('http://localhost:3334/api/login/start', null, {
+        withCredentials: true,
+      })
+      .subscribe((res: any) => {
+        location.href = res.authorizationRequestUrl;
+      });
+  }
+
+  public updateUserInfo(): void {
+    this.http
+      .get('http://localhost:3334/api/user-info', {
+        withCredentials: true,
+      })
+      .subscribe((res: any) => {
+        this.userInfo$.next(res);
+      });
+  }
+
+  public logout(): void {
+    console.log('logout');
+  }
 }
