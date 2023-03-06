03b75c051c4f2fcbff059b42c0e7c24f57041b91
Marley Powell <marley.powell@exclaimer.com>
2023-03-06 09:19:12 +0000

    feat: :sparkles: added angular material

--- apps/app1/src/main.ts
+++ apps/app1/src/main.ts
@@ -1,11 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import {
-  provideRouter,
-  withEnabledBlockingInitialNavigation,
-} from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    importProvidersFrom(HttpClientModule, BrowserAnimationsModule),
+  ],
 }).catch((err) => console.error(err));
