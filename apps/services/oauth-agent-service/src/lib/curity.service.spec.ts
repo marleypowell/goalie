140513bf7b5169838edb1c22ff54b6662ec4c346
Marley Powell <marley.powell@exclaimer.com>
2023-03-06 14:22:59 +0000

    feat: :sparkles: created curity service

---
+++ apps/services/oauth-agent-service/src/lib/curity.service.spec.ts
@@ -0,0 +1,112 @@
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AxiosResponse } from 'axios';
+import { firstValueFrom, of } from 'rxjs';
+import { CurityService } from './curity.service';
+
+describe(CurityService.name, () => {
+  let service: CurityService;
+  let configService: ConfigService;
+  let http: HttpService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
+      providers: [CurityService, ConfigService],
+    }).compile();
+
+    service = module.get<CurityService>(CurityService);
+    configService = module.get<ConfigService>(ConfigService);
+    http = module.get<HttpService>(HttpService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should get a token', async () => {
+    jest.spyOn(configService, 'get').mockReturnValueOnce('https://localhost:8443/oauth2/token');
+    jest.spyOn(configService, 'get').mockReturnValueOnce('clientId');
+    jest.spyOn(configService, 'get').mockReturnValueOnce('clientSecret');
+    jest.spyOn(configService, 'get').mockReturnValueOnce('http://localhost:4200/login');
+
+    const data = { access_token: 'accessToken', id_token: 'idToken' };
+    const postSpy = jest.spyOn(http, 'post').mockReturnValue(of({ data } as AxiosResponse));
+
+    const response = await firstValueFrom(service.getToken('code', 'codeVerifier'));
+
+    expect(response).toEqual(data);
+    expect(postSpy).toHaveBeenCalledWith(
+      'https://localhost:8443/oauth2/token',
+      'grant_type=authorization_code&redirect_uri=http://localhost:4200/login&code=code&code_verifier=codeVerifier',
+      expect.objectContaining({
+        headers: {
+          Authorization: 'Basic Y2xpZW50SWQ6Y2xpZW50U2VjcmV0',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+    );
+  });
+
+  it('should throw an error when the token endpoint is not configured', async () => {
+    jest.spyOn(configService, 'get').mockReturnValueOnce(undefined);
+    await expect(firstValueFrom(service.getToken('code', 'codeVerifier'))).rejects.toThrow();
+  });
+
+  it('should get a refresh token', async () => {
+    jest.spyOn(configService, 'get').mockReturnValueOnce('https://localhost:8443/oauth2/token');
+    jest.spyOn(configService, 'get').mockReturnValueOnce('clientId');
+    jest.spyOn(configService, 'get').mockReturnValueOnce('clientSecret');
+
+    const data = { access_token: 'accessToken', id_token: 'idToken' };
+    const postSpy = jest.spyOn(http, 'post').mockReturnValue(of({ data } as AxiosResponse));
+
+    const refreshToken = 'refreshToken';
+
+    const response = await firstValueFrom(service.getRefreshToken(refreshToken));
+    expect(response).toEqual(data);
+    expect(postSpy).toHaveBeenCalledWith(
+      'https://localhost:8443/oauth2/token',
+      'grant_type=refresh_token&refresh_token=refreshToken',
+      expect.objectContaining({
+        headers: {
+          Authorization: 'Basic Y2xpZW50SWQ6Y2xpZW50U2VjcmV0',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+    );
+  });
+
+  it('should throw an error when the token endpoint is not configured', async () => {
+    jest.spyOn(configService, 'get').mockReturnValueOnce(undefined);
+    await expect(firstValueFrom(service.getRefreshToken('refreshToken'))).rejects.toThrow();
+  });
+
+  it('should get the user info', async () => {
+    jest.spyOn(configService, 'get').mockReturnValueOnce('https://localhost:8443/oauth2/userinfo');
+
+    const data = { id: 'userId' };
+    const postSpy = jest.spyOn(http, 'post').mockReturnValue(of({ data } as AxiosResponse));
+
+    const accessToken = 'accessToken';
+
+    const response = await firstValueFrom(service.getUserInfo(accessToken));
+    expect(response).toEqual(data);
+    expect(postSpy).toHaveBeenCalledWith(
+      'https://localhost:8443/oauth2/userinfo',
+      null,
+      expect.objectContaining({
+        headers: {
+          Authorization: 'Bearer accessToken',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+    );
+  });
+
+  it('should throw an error when the userinfo endpoint is not configured', async () => {
+    jest.spyOn(configService, 'get').mockReturnValueOnce(undefined);
+    await expect(firstValueFrom(service.getUserInfo('accessToken'))).rejects.toThrow();
+  });
+});
---
+++ apps/services/oauth-agent-service/src/lib/curity.service.ts
@@ -0,0 +1,151 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { catchError, map, Observable } from 'rxjs';
+import AuthorizationClientException from './exceptions/AuthorizationClientException';
+import AuthorizationServerException from './exceptions/AuthorizationServerException';
+import { OAuthAgentException } from './exceptions/OAuthAgentException';
+import { Grant } from './grant';
+import { TokenResponse } from './token-response';
+import { UserInfo } from './user-info';
+
+/**
+ * Service for interacting with the Curity OAuth 2.0 server.
+ */
+@Injectable()
+export class CurityService {
+  public constructor(private readonly configService: ConfigService, private readonly http: HttpService) {}
+
+  /**
+   * Get a token from the token endpoint using the Authorization Code Grant.
+   * @param code the authorization code.
+   * @param codeVerifier the code verifier used to generate the code challenge.
+   * @returns the token response.
+   */
+  public getToken(code: string, codeVerifier: string): Observable<TokenResponse> {
+    const tokenEndpoint = this.configService.get<string>('tokenEndpoint');
+    const clientID = this.configService.get<string>('clientID');
+    const clientSecret = this.configService.get<string>('clientSecret');
+    const redirectUri = this.configService.get<string>('redirectUri');
+
+    return this.http
+      .post<TokenResponse>(
+        tokenEndpoint,
+        `grant_type=authorization_code&redirect_uri=${redirectUri}&code=${code}&code_verifier=${codeVerifier}`,
+        {
+          headers: {
+            Authorization: `Basic ${Buffer.from(clientID + ':' + clientSecret).toString('base64')}`,
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        }
+      )
+      .pipe(
+        map((res) => {
+          if (res.status >= 500) {
+            const error = new AuthorizationServerException();
+            error.logInfo = `Server error response in an Authorization Code Grant: ${res.data}`;
+            throw error;
+          }
+
+          if (res.status >= 400) {
+            throw new AuthorizationClientException(Grant.AuthorizationCode, res.status, String(res.data));
+          }
+
+          return res.data;
+        }),
+        catchError((err) => {
+          if (!(err instanceof OAuthAgentException)) {
+            const error = new AuthorizationServerException(err);
+            error.logInfo = 'Connectivity problem during an Authorization Code Grant';
+            throw error;
+          } else {
+            throw err;
+          }
+        })
+      );
+  }
+
+  /**
+   * Get a token from the token endpoint using the Refresh Token Grant.
+   * @param refreshToken the refresh token.
+   * @returns the token response.
+   */
+  public getRefreshToken(refreshToken: string): Observable<TokenResponse> {
+    const tokenEndpoint = this.configService.get<string>('tokenEndpoint');
+    const clientID = this.configService.get<string>('clientID');
+    const clientSecret = this.configService.get<string>('clientSecret');
+
+    return this.http
+      .post<TokenResponse>(tokenEndpoint, `grant_type=refresh_token&refresh_token=${refreshToken}`, {
+        headers: {
+          Authorization: `Basic ${Buffer.from(clientID + ':' + clientSecret).toString('base64')}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+      .pipe(
+        map((res) => {
+          if (res.status >= 500) {
+            const error = new AuthorizationServerException();
+            error.logInfo = `Server error response in an Authorization Code Grant: ${res.data}`;
+            throw error;
+          }
+
+          if (res.status >= 400) {
+            throw new AuthorizationClientException(Grant.AuthorizationCode, res.status, String(res.data));
+          }
+
+          return res.data;
+        }),
+        catchError((err) => {
+          if (!(err instanceof OAuthAgentException)) {
+            const error = new AuthorizationServerException(err);
+            error.logInfo = 'Connectivity problem during an Authorization Code Grant';
+            throw error;
+          } else {
+            throw err;
+          }
+        })
+      );
+  }
+
+  /**
+   * Get user info from the user info endpoint.
+   * @param accessToken the access token.
+   * @returns the user info.
+   */
+  public getUserInfo(accessToken: string): Observable<UserInfo> {
+    const userInfoEndpoint = this.configService.get<string>('userInfoEndpoint');
+
+    return this.http
+      .post<UserInfo>(userInfoEndpoint, null, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+      .pipe(
+        map((res) => {
+          if (res.status >= 500) {
+            const error = new AuthorizationServerException();
+            error.logInfo = `Server error response in an Authorization Code Grant: ${res.data}`;
+            throw error;
+          }
+
+          if (res.status >= 400) {
+            throw new AuthorizationClientException(Grant.AuthorizationCode, res.status, String(res.data));
+          }
+
+          return res.data;
+        }),
+        catchError((err) => {
+          if (!(err instanceof OAuthAgentException)) {
+            const error = new AuthorizationServerException(err);
+            error.logInfo = 'Connectivity problem during a User Info request';
+            throw error;
+          } else {
+            throw err;
+          }
+        })
+      );
+  }
+}
