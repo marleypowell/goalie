b29de41ff399bd8412cc75f84d4cbd033caff6a7
Marley Powell <marley.powell@exclaimer.com>
2023-03-16 14:29:45 +0000

    ci: :construction_worker: updated docker build pipeline

--- apps/services/oauth-agent-service/src/main.ts
+++ apps/services/oauth-agent-service/src/main.ts
@@ -3,16 +3,15 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
-import { Config } from './config/config.interface';
 import { CookieEncryptionService } from './lib/cookie-encryption.service';
 import { nestCsrf } from './lib/csrf.middleware';
 import { setupSwagger } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = app.get(ConfigService<Config>);
+  const config = app.get(ConfigService);
 
-  const endpointsPrefix = config.get('endpointsPrefix', { infer: true });
+  const endpointsPrefix = config.get<string>('endpointsPrefix', { infer: true });
   app.setGlobalPrefix(endpointsPrefix);
 
   app.enableCors({
@@ -22,12 +21,12 @@ async function bootstrap() {
   app.use(cookieParser());
 
   const cookieEncryptionService = app.get(CookieEncryptionService);
-  const cookieNamePrefix = config.get('cookieNamePrefix', { infer: true });
+  const cookieNamePrefix = config.get<string>('cookieNamePrefix', { infer: true });
   app.use(nestCsrf({ cookieEncryptionService, cookieName: `${cookieNamePrefix}-csrf` }));
 
   setupSwagger(app);
 
-  const port = config.get('port', { infer: true });
+  const port = config.get<number>('port', { infer: true });
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}${endpointsPrefix}`);
 }
