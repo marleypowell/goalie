90dba0c36eb6d19cfcd34f480df67f47256589f7
Marley Powell <marley.powell@exclaimer.com>
2023-03-17 14:57:30 +0000

    feat(bff): :loud_sound: added debug logging to `JwtAuthGuard`

--- apps/app1-bff/src/app/auth/jwt-auth.guard.ts
+++ apps/app1-bff/src/app/auth/jwt-auth.guard.ts
@@ -1,16 +1,24 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, Logger } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 import { Observable } from 'rxjs';
 import { ALLOW_ANONYMOUS } from './allow-anonymous.decorator';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  private readonly logger = new Logger(JwtAuthGuard.name);
+
   public constructor(private reflector: Reflector) {
     super();
   }
 
   public override canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    this.logger.debug(
+      `Checking if request is allowed to be anonymous`,
+      JSON.stringify(context.switchToHttp().getRequest<Request>().rawHeaders)
+    );
+
     const allowAnonymous = this.reflector.getAllAndOverride<boolean>(ALLOW_ANONYMOUS, [
       context.getHandler(),
       context.getClass(),
