39a8273b1f57d1fcf8ce369d121205d7c9f697f8
Marley Powell <marley.powell@exclaimer.com>
2023-03-17 17:57:04 +0000

    fix: :bug: updated xsrf interceptor

--- apps/app1/src/app/interceptors/xsrf.interceptor.ts
+++ apps/app1/src/app/interceptors/xsrf.interceptor.ts
@@ -1,11 +1,20 @@
 import { HttpClient, HttpEvent, HttpHandlerFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { mergeMap, Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 const HEADER_NAME = 'x-goalie-csrf';
 
 export function xsrfInterceptorFn(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
-  if (req.method === 'GET' || req.method === 'HEAD') {
+  const lcUrl = req.url.toLowerCase();
+
+  if (
+    req.method === 'GET' ||
+    req.method === 'HEAD' ||
+    lcUrl.startsWith('http://') ||
+    lcUrl.startsWith('https://') ||
+    (environment.basePath && !lcUrl.startsWith(environment.basePath))
+  ) {
     return next(req);
   }
 
