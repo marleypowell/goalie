163affe94378f29c80c4ba476af7d30cb171ac7c
Marley Powell <marley.powell@exclaimer.com>
2023-03-21 08:26:14 +0000

    refactor: :truck: renamed `app1` and `app1-bff` to `frontend` and `api-gateway`

--- apps/frontend-e2e/src/e2e/app.cy.ts
+++ apps/frontend-e2e/src/e2e/app.cy.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('app1', () => {
+describe('frontend', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('app1', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome app1');
+    getGreeting().contains('Welcome frontend');
   });
 });
--- apps/frontend/jest.config.ts
+++ apps/frontend/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'app1',
+  displayName: 'frontend',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/app1',
+  coverageDirectory: '../../coverage/apps/frontend',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
