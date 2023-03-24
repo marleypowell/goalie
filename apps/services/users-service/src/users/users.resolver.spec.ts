1a5966f737938a1d4045203e5c39faa5c6d210d0
Marley Powell <marley.powell@exclaimer.com>
2023-03-24 14:32:32 +0000

    test: :white_check_mark: fixed test issue

--- apps/services/users-service/src/users/users.resolver.spec.ts
+++ apps/services/users-service/src/users/users.resolver.spec.ts
@@ -1,13 +1,14 @@
+import { CqrsModule } from '@nestjs/cqrs';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersResolver } from './users.resolver';
-import { UsersService } from './users.service';
 
 describe('UsersResolver', () => {
   let resolver: UsersResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersResolver, UsersService],
+      imports: [CqrsModule],
+      providers: [UsersResolver],
     }).compile();
 
     resolver = module.get<UsersResolver>(UsersResolver);
