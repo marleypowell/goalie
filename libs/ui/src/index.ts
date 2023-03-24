435e15d5e4fcda66b39ef731de45e0fb740de706
Marley Powell <marley.powell@exclaimer.com>
2023-03-24 10:10:09 +0000

    fix: :bug: added missing export

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/create-goal-form/create-goal-form.component';
 export * from './lib/goal-details/goal-details.component';
 export * from './lib/goals-list/goals-list.component';
+export * from './lib/goals-table/goals-table.component';
 export * from './lib/header/header.component';
 export * from './lib/user-goals/user-goals.component';
